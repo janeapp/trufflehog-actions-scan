f5992d42f3b5e7b8472ca7ca35076413afb806df
Ben Gibbs <ben@bobop.ca>
2022-05-10 08:26:55 -0700

    Make Github Action workspace a safe directory for git
    See https://github.com/actions/checkout/issues/760

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,6 +10,10 @@ if [ -n "${INPUT_ALLOWED_PATH}" ]; then
   args="${args} --allow ${INPUT_ALLOWED_PATH}"
 fi
 
+# Make workspace a safe directory for git
+# See https://github.com/actions/checkout/issues/760
+git config --global --add safe.directory /github/workspace
+
 cp /regexes.json .
 trufflehog . ${args} | tee trufflehog_output.log
 
